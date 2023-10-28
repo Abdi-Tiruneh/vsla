7372bc66702f85851e74037a610afe87848541f5
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-10-28 10:46:47 +0300

    first commit: project setup

---
+++ src/main/java/vsla/VslaApplication.java
@@ -0,0 +1,13 @@
+package vsla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VslaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VslaApplication.class, args);
+	}
+
+}
